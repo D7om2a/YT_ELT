447dcb27ee359b795df9d6f54cf0ddddda21b3cc
D7om2a <D7om2a@gmail.com>
2026-04-01 17:12:33 +0300

    adding docker-compose.yaml file

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,15 +2,21 @@ import requests
 import json
 from datetime import date
 
-import os 
-from dotenv import load_dotenv
+#import os 
+#from dotenv import load_dotenv
+#load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
+
+@task
 def get_playlist_id():
 
     try:
@@ -42,7 +48,7 @@ def get_playlist_id():
     
 
 
-
+@task
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -82,7 +88,7 @@ def get_video_ids(playlistId):
     
 
 
-
+@task
 def extract_video_data(video_ids):
     extracted_data = []
 
@@ -130,7 +136,7 @@ def extract_video_data(video_ids):
         raise e
 
 
-
+@task
 def save_to_json(extracted_data):
     file_path = f"data/YT_data_{date.today()}.json"
 
