ddb44a4c9f50a0b288b589363e15842c0757a281
D7om2a <D7om2a@gmail.com>
2026-04-24 12:32:22 +0300

    adding  data quality to airflow

--- dags/main.py
+++ dags/main.py
@@ -3,6 +3,7 @@ import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 
 
@@ -23,6 +24,10 @@ default_args = {
     "start_date": datetime(2026, 1, 1, tzinfo=local_tz),
     #"end_date": datetime(2030, 12, 31, tzinfo=local_tz),
 }
+
+# Variables
+staging_schema = "staging"
+core_schema = "core"
 
 with DAG(
     dag_id = 'produce_json',
@@ -54,3 +59,17 @@ with DAG(
 
     # define dependencies
     update_staging >> update_core
+
+
+    with DAG(
+        dag_id = "data_quality",
+        default_args=default_args,
+        description = "DAG to check th data quality on both layers in the db",
+        schedule='0 16 * * *',
+        catchup=False
+)   as dag_update:
+        # define the tasks
+        soda_validate_staging = yt_elt_data_quality(staging_schema)
+        soda_validate_core = yt_elt_data_quality(core_schema)
+        # define dependencies
+        soda_validate_staging >> soda_validate_core
